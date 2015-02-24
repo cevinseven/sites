39f6fdbe8f7a734fe976297c268a96801935a598
Cevin Williamson <cevin@fatsamurai.com>
2015-02-24 12:12:34 -0600

    adjustments

--- rework/wwwroot/js/main.js
+++ rework/wwwroot/js/main.js
@@ -1,2 +1,37 @@
 $(document).ready(function () {
+  var $flex = $('.flexslider');
+
+  $flex.flexslider({
+    slideshow: true,
+    animation: 'slide',
+    directionNav: true,
+    controlNav: false,
+    slideshowSpeed: 4000,
+    animationSpeed: 1000,
+    pauseOnHover: true,
+    after: function () {
+      $('.slide-nav li')
+        .removeClass('active')
+        .eq( $flex.data('flexslider').currentSlide )
+        .addClass('active');
+    }
+});
+
+  $('.slide-nav').delegate('li', 'click', function() {
+
+    var slide = {
+      target: $(this).index(),
+      current: $flex.data('flexslider').currentSlide
+    }
+
+    if ( $flex.data('flexslider').animating === false){
+      if (slide.target !== slide.current) {
+        $('.slide-nav li')
+          .removeClass('active')
+          .eq(slide.target)
+          .addClass('active');
+        $flex.data('flexslider').flexAnimate(slide.target);
+      }
+    }
+  });
 });
