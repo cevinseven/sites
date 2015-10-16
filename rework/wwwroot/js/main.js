547ddd85cd7567071b209cc17b9dadfb3b29778b
Cevin Williamson <cevin@fatsamurai.com>
2015-10-16 18:26:29 -0500

    stripped down

--- rework/wwwroot/js/main.js
+++ rework/wwwroot/js/main.js
@@ -1,37 +1,45 @@
 $(document).ready(function () {
-  var $flex = $('.flexslider');
+ 
+$('.nav-bar .btn-group a').click(function() {
 
-  $flex.flexslider({
-    slideshow: false,
-    animation: 'slide',
-    directionNav: true,
-    controlNav: false,
-    slideshowSpeed: 5000,
-    animationSpeed: 2000,
-    pauseOnHover: true,
-    after: function () {
-      $('.slide-nav li')
-        .removeClass('active')
-        .eq( $flex.data('flexslider').currentSlide )
-        .addClass('active');
-    }
-});
 
-  $('.slide-nav').delegate('li', 'click', function() {
-
-    var slide = {
-      target: $(this).index(),
-      current: $flex.data('flexslider').currentSlide
-    }
-
-    if ( $flex.data('flexslider').animating === false){
-      if (slide.target !== slide.current) {
-        $('.slide-nav li')
-          .removeClass('active')
-          .eq(slide.target)
-          .addClass('active');
-        $flex.data('flexslider').flexAnimate(slide.target);
-      }
-    }
+    $('.page').html('.about');
   });
+
 });
+  // var theDiv = $(this).attr("href");
+//   var $flex = $('.flexslider');
+
+//   $flex.flexslider({
+//     slideshow: false,
+//     animation: 'slide',
+//     directionNav: true,
+//     controlNav: false,
+//     slideshowSpeed: 5000,
+//     animationSpeed: 2000,
+//     pauseOnHover: true,
+//     after: function () {
+//       $('.slide-nav li')
+//         .removeClass('active')
+//         .eq( $flex.data('flexslider').currentSlide )
+//         .addClass('active');
+//     }
+// });
+
+//   $('.slide-nav').delegate('li', 'click', function() {
+
+//     var slide = {
+//       target: $(this).index(),
+//       current: $flex.data('flexslider').currentSlide
+//     }
+
+//     if ( $flex.data('flexslider').animating === false){
+//       if (slide.target !== slide.current) {
+//         $('.slide-nav li')
+//           .removeClass('active')
+//           .eq(slide.target)
+//           .addClass('active');
+//         $flex.data('flexslider').flexAnimate(slide.target);
+//       }
+//     }
+//   });
